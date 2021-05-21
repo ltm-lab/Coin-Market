b34d30c0b54e42fbb4ed50c462e7fde75d822b26
ltm-lab <taemin328@naver.com>
2021-05-22 04:20:30 +0900

    Create shop.java
    패키지 생성 및 shop.java 파일 생성

---
+++ KMUbit/shop.java
@@ -0,0 +1,28 @@
+package KMUbit;
+
+class gameui {
+	static void title() {
+		System.out.println("==================================================================");
+		System.out.println("|                          KMUbit(v1.0)                          |");
+		System.out.println("==================================================================");
+	}
+
+	static void crntcoin() {
+		System.out.println("|      코인      |    현재시세    |   보유개수   |   평가금액   |  등락폭  |");
+		System.out.println("------------------------------------------------------------------");
+		System.out.println("|      코인      |    현재시세    |   보유개수   |   평가금액   |  등락폭  |");
+		System.out.println("|      코인      |    현재시세    |   보유개수   |   평가금액   |  등락폭  |");
+		System.out.println("|      코인      |    현재시세    |   보유개수   |   평가금액   |  등락폭  |");
+		System.out.println("|      코인      |    현재시세    |   보유개수   |   평가금액   |  등락폭  |");
+		System.out.println("|      코인      |    현재시세    |   보유개수   |   평가금액   |  등락폭  |");
+		System.out.println("|      코인      |    현재시세    |   보유개수   |   평가금액   |  등락폭  |");
+		System.out.println("==================================================================");
+	}
+}
+
+public class shop extends gameui {
+	public static void main(String[] args) {
+		title();
+		crntcoin();
+	}
+}
