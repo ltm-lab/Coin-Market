9a85e65af7b43bcb9f5460810ef0280979b7b3bc
ltm-lab <taemin328@naver.com>
2021-06-04 00:16:23 +0900

    add buy_coin, sell_coin
    buy_coin, sell_coin 함수 추가

--- src/KMUbit/Shop.java
+++ src/KMUbit/Shop.java
@@ -1,5 +1,43 @@
 package KMUbit;
 
 public class Shop extends Coin {
+	static void buy_coin(int coin, int cnt) {
+		if (coin > 0 && coin < 7) {
+			int useMoney = price[coin - 1] * cnt;
 
+			if (Player.money >= useMoney) {
+				Player.money -= useMoney;
+				Player.my_coins[coin - 1] += cnt;
+			} else {
+				System.out.println("소지금이 적어 코인 매수에 실패하였습니다.");
+			}
+		} else {
+			System.out.println("해당 번호에 해당하는 코인이 없어 매수에 실패하였습니다.");
+		}
+	}
+
+	static void sell_coin(int coin, int cnt) {
+		if (coin > 0 && coin < 7) {
+			int useMoney = price[coin - 1] * cnt;
+
+			if (Player.my_coins[coin - 1] >= cnt) {
+				Player.my_coins[coin - 1] -= cnt;
+				Player.money += useMoney;
+			} else {
+				System.out.println("현재 보유하고 있는 코인보다 많은 코인을 매도할 수 없습니다.");
+			}
+		} else {
+			System.out.println("해당 번호에 해당하는 코인이 없어 매도에 실패하였습니다.");
+		}
+	}
+
+	static void buy_hint(int coin, int level) {
+		if (level == 1) {
+
+		} else if (level == 2) {
+
+		} else {
+
+		}
+	}
 }
